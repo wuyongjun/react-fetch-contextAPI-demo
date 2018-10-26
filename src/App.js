3953821acc601a27eaff9621fa5ac1d03cd5f8eb
wuyongjun <wuyongjuntie2013@163.com>
2018-10-26 13:30:49 +0800

    commit code

--- src/App.js
+++ src/App.js
@@ -3,23 +3,85 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    isLoading: false,
+    data: null
+  }
+
+  // 使用setState的异步回调函数
+  // fetchDate () {
+  //   this.setState({
+  //     isLoading: true
+  //   }, () => {
+  //     fetch('https://api.github.com/users/mojombo')
+  //       .then(response => response.json())
+  //       .then(data => {
+  //         setInterval(() => {
+  //           this.setState({
+  //             data,
+  //             isLoading: false
+  //           });
+  //         }, 3000);
+  //       });
+  //   })
+  // }
+
+  // 用Promise对象封装的setState方法
+  // fetchDate () {
+  //   this.setStateAsync({isLoading: true})
+  //     .then(() => {
+  //       fetch('https://api.github.com/users/mojombo')
+  //         .then(res => res.json())
+  //         .then(data => {
+  //           setInterval(() => {
+  //             this.setState({
+  //               data,
+  //               isLoading: false
+  //             });
+  //           }, 3000);
+  //         });
+  //     });
+  // }
+  
+  // 用Promise对象封装setState方法
+  setStateAsync = (state) => {
+    return new Promise(resolve => {
+      this.setState(state, resolve);
+    });
+  }
+
+  async fetchDate () {
+    await this.setStateAsync({ isLoading: true });
+    let res = await fetch('https://api.github.com/users/mojombo');
+    let data = await res.json();
+    setInterval(() => {
+      this.setStateAsync({ data, isLoading: false});
+    }, 3000);
+  }
+
   render() {
+    let { isLoading, data } = this.state;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h3>welcome to react world</h3>
         </header>
+        <button type="button" onClick={this.fetchDate.bind(this)}>获取数据</button>
+        {
+          isLoading ? <p>加载中...</p> : (<div>
+            {
+              data === null ? <p>暂无数据</p> : (<div>
+                <ul>
+                  <li>姓名：{data.login}</li>
+                  <li>博客：{data.blog}</li>
+                  <li>加入时间：{data.created_at}</li>
+                </ul>
+              </div>)
+            }
+          </div>)
+        }
+        
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,105 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// fetch交互案例在app.js文件中，跨域可以使用别人封装的库---fetch-jsonp
+
+// context api v16.3.0后添加，用于统一管理状态，实现跨组件间的状态管理和修改
+let { Provider, Consumer } = createContext();
+
+// 提供一个统一管理状态的组件
+class ProviderComponent extends React.Component {
+
+    state = {
+        name: 'mike',
+        age: 20
+    }
+
+    changeAge = (age) => {
+        this.setState({age});
+    }
+
+    render () {
+        return (
+            // 此处value名字为固定 redux此处的属性值可能是store
+            <Provider value={{
+                state: this.state,
+                changeAge: this.changeAge
+            }}>
+                {this.props.children}
+            </Provider>
+        );
+    }
+}
+
+class Child extends React.Component {
+    constructor (props) {
+        super(props);
+    }
+
+    state = {
+        chlidName: 'lily',
+        age: 18
+    }
+
+    changeAge (ev) {
+        this.setState({age: ev.target.value})
+    }
+
+    keySend = (fn, ev) => {
+        if (ev.which === 13) {
+            fn(this.state.age);
+        }
+    }
+
+    render () {
+        return (<div>
+            {/* 此处为固定写法 */}
+            
+            <Consumer>
+                {
+                    (value) => {
+                        return (<div>
+                            <h3>{JSON.stringify(value)}</h3>
+                            {/* <button onClick={() => value.changeAge(this.state.age)}>更改年龄</button> */}
+                            <input type="text" value={this.state.age} onChange={this.changeAge.bind(this)} 
+                                onKeyDown={(ev) => this.keySend(value.changeAge, ev)} />
+                        </div>);
+                    }
+                }
+            </Consumer>
+        </div>);
+    }
+}
+
+class Child2 extends React.Component {
+    render () {
+        return(<div>
+            <Consumer>
+                {
+                    (value) => (<h2>age: {value.state.age}</h2>)
+                }
+            </Consumer>
+        </div>)
+    }
+}
+
+class App extends React.Component {
+    render () {
+        return (<div>
+            <h3>hello react</h3>
+            <Child></Child>
+            <Child2></Child2>
+        </div>);
+    }
+}
+
+
+ReactDOM.render((<ProviderComponent>
+    <App />
+</ProviderComponent>), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
